e0509eda6df2d50cda29a1f3e9e4c36027c30b26
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-05-02 10:40:52 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,26 +1,27 @@
 function dijkstra(graph, sourceNode) {
     let distances = {};
     let visited = new Set();
+    
+    for (let node in graph) {
+        distances[node] = Infinity;
+    }
+    distances[sourceNode] = 0;
 
-for (let node in graph) {
-    distances[node] = Infinity;
+    while (Object.keys(graph).length !== visited.size) {
+        let unvisitedNodes = Object.keys(graph).filter(node => !visited.has(node));
+        let closestNode = unvisitedNodes.reduce((a, b) => distances[a] < distances[b] ? a : b);
 
-distances[sourceNode] = 0;
+        visited.add(closestNode);
 
-while (Object.keys(graph).length !== visited.size) {
-    let unvisitedNodes = Object.keys(graph).filter(node => !visited.has(node));
-    let closestNode = unvisitedNodes.reduce((a, b) => distances[a] < distances[b] ? a : b);
-
-    visited.add(closestNode);
-
-    for (let neighbor in graph[closestNode]) {
-        let alternatePathDistance = distances[closestNode] + graph[closestNode][neighbor];
-        if (alternatePathDistance < distances[neighbor]) {
-            distances[neighbor] = alternatePathDistance;
+        for (let neighbor in graph[closestNode]) {
+            let alternatePathDistance = distances[closestNode] + graph[closestNode][neighbor];
+            if (alternatePathDistance < distances[neighbor]) {
+                distances[neighbor] = alternatePathDistance;
+            }
         }
     }
+
+    return distances;
 }
-}
-    return [];
-}
+
 module.exports = dijkstra;
