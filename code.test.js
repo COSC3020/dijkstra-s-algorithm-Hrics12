89463562579595bfce624702c0439af588efb3e2
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-05-05 10:55:27 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -9,4 +9,8 @@ test('Test Dijkstra function', () => {
     };
 
     expect(dijkstra(graph, 'A')).toEqual({'A': 0, 'B': 1, 'C': 3, 'D': 4});
+    expect(dijkstra(graph, 'B')).toEqual({ 'A': 1, 'B': 0, 'C': 2, 'D': 3 });
+    expect(dijkstra(graph, 'C')).toEqual({ 'A': 3, 'B': 2, 'C': 0, 'D': 1 });
+    expect(dijkstra(graph, 'D')).toEqual({ 'A': 4, 'B': 3, 'C': 1, 'D': 0 });
+ 
 });
